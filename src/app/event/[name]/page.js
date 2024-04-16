3b9f4d48c0c35e97fc19ac7ede3ddeae8d6c44d6
NAMANIND <namanrai309@gmail.com>
2024-04-16 16:19:19 +0530

    email

--- src/app/event/[name]/page.js
+++ src/app/event/[name]/page.js
@@ -14,11 +14,16 @@ function page({ params }) {
   if (data.includes(name)) {
     const to =
       name === "megha"
-        ? ["20bei033@ietdavv.edu.in", "megha.salian@influenceexchangegroup.com"]
+        ? [
+            "20bei033@ietdavv.edu.in",
+            "megha.salian@influenceexchangegroup.com",
+            "mohamed.suhel@influenceexchangegroup.com",
+          ]
         : [
             "20bei033@ietdavv.edu.in",
             "megha.salian@influenceexchangegroup.com",
             "navya.kotian@influenceexchangegroup.com",
+            "mohamed.suhel@influenceexchangegroup.com",
           ];
 
     return (
