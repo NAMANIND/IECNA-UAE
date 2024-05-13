582ebc331b4fa6e26b6447a254806bd1609a31ab
NAMANIND <namanrai309@gmail.com>
2024-05-13 23:59:52 +0530

    shd

--- src/components/prform/NewMultiPageForm.js
+++ src/components/prform/NewMultiPageForm.js
@@ -536,14 +536,14 @@ const NewMultiPageForm = ({ to, name }) => {
         <form
           onSubmit={handleSubmit}
           className=" sm:w-[60%] w-[100%]  sm:p-16 p-8   rounded-[32px] "
-          style={{
-            boxShadow: "0px 0px 10px 0px #0000001a ",
-            background: "rgba(255, 255, 255, 1.15) ",
-            border: "1px solid #0000001a",
-            color: "rgba(0, 0, 0, 0.18)",
-            filter: "drop-shadow(40px 40px 76px)",
-            mixBlendMode: "normal",
-          }}
+          // style={{
+          //   boxShadow: "0px 0px 10px 0px #0000001a ",
+          //   background: "rgba(255, 255, 255, 1.15) ",
+          //   border: "1px solid #0000001a",
+          //   color: "rgba(0, 0, 0, 0.18)",
+          //   filter: "drop-shadow(40px 40px 76px)",
+          //   mixBlendMode: "normal",
+          // }}
         >
           {page === 1 && (
             <div className="flex  flex-col md:flex-nowrap gap-4">
