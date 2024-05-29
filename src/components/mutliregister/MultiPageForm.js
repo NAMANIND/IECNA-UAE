9031edb9ebe02cc4af47f57db36c3c2b81f3bcb5
NAMANIND <namanrai309@gmail.com>
2024-05-29 12:18:01 +0530

    done

--- src/components/mutliregister/MultiPageForm.js
+++ src/components/mutliregister/MultiPageForm.js
@@ -195,9 +195,6 @@ const MultiPageForm = () => {
       const to = [
         "mohamed.suhel@influenceexchangegroup.com",
         "20bei033@ietdavv.edu.in",
-        // "mohamed.suhel@influenceexchangegroup.com ",
-
-        "megha.salian@influenceexchangegroup.com",
       ];
       // "megha.salian@influenceexchangegroup.com",
       const subject =
